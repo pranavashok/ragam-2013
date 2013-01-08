f0a6e91bd3bfd3f8fadfcde68eee17887c4f158d
root <sachu92@gmail.com>
2013-01-08 18:46:35 +0530

    submenu pane fixed

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ function initialize()
 {
 	$("#mainmenu-pane").animate({right:'70%'},'slow', function(){ $("#mainmenu-pane").rotate({angle:0, animateTo:-25})});
 	$("#font-pane").animate({left:'30%'}, 'slow', function(){ $("#font-pane").rotate({angle:0, animateTo:-25})});
-	$("#submenu-pane").rotate({angle:-25});
-	$("#contents-pane").rotate({angle:-25});
+	$("#submenu-pane").rotate({angle:-15});
+	$("#contents-pane").rotate({angle:-15});
 }
 $(document).ready(function(){
 			initialize();
