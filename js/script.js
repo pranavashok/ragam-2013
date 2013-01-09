38cc4e5cf0fd63639b9ce28e0adedafcd1a72846
Shamil C M <shamil.cm@gmail.com>
2013-01-09 15:32:51 +0530

    links

--- js/script.js
+++ js/script.js
@@ -9,10 +9,13 @@ $(document).ready(function(){
 			var w = $(window).width();
 			var mstate = 0;
 			initialize();
-			$("#mainlinks a").click(function(){
+			$("#mainlinks li").click(function(){
+				if(mstate == 0)
+				{
 				$("#font-pane").css({left: w*0.3+'px'});
 				$("#font-pane").stop().animate({left:'100%', top:'-160%'}, 1500, 'easeOutQuart');
 				$("#mainmenu-pane").stop().animate({right:'100%', top:'-70%'}, 1500, 'easeOutQuart', function() { mstate = 1; });
+				}
 			});
 			$("#mainmenu-pane").click(
 				function(){
