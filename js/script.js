53bfb5098b1c1a49c9113ed63dd6c0ca15a4a0d3
Pranav Ashok <pranavashok@gmail.com>
2013-01-09 15:30:17 +0530

    Inner animations

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function initialize()
 	$("#mainmenu-pane").animate({right:'70%'},'slow', 'easeInOutQuad', function(){ $("#mainmenu-pane").rotate({angle:0, animateTo:-25})});
 	$("#font-pane").animate({left:'30%'}, 'slow', 'easeInOutQuad', function(){ $("#font-pane").rotate({angle:0, animateTo:-25})});
 	$("#submenu-pane").rotate({angle:-15});
-	$("#contents-pane").rotate({angle:-15});
+	$("#content-pane").rotate({angle:-15});
 }
 $(document).ready(function(){
 			var w = $(window).width();
@@ -11,9 +11,14 @@ $(document).ready(function(){
 			initialize();
 			$("#mainlinks a").click(function(){
 				$("#font-pane").css({left: w*0.3+'px'});
-				$("#font-pane").stop().animate({left:'100%', top:'-160%'}, 1500, 'easeOutQuart');
-				$("#mainmenu-pane").stop().animate({right:'100%', top:'-70%'}, 1500, 'easeOutQuart', function() { mstate = 1; });
+				$("#font-pane").stop().animate({left:'100%', top:'-160%'}, 1000, 'easeInOutQuad');
+				$("#mainmenu-pane").stop().animate({right:'105%', top:'-70%'}, 1000, 'easeInOutQuad', 
+					function() { 
+						mstate = 1;
+					});
 			});
+			/* Code for bringbing back homepage */
+			/*
 			$("#mainmenu-pane").click(
 				function(){
 					if(mstate == 1) {
@@ -23,4 +28,5 @@ $(document).ready(function(){
 						$("#font-pane").css({left: w*0.3+'px'});
 					}
 				});
+			*/
 });
