bb1e6a71f5d39eacabc42e0be8a8e52ad21cef82
Pranav Ashok <pranavashok@gmail.com>
2013-01-11 16:13:45 +0530

    Fixed puller not showing on close

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ $(document).ready(function(){
 			container.removeClass('support');
 			$("#mainmenu-pane").removeClass('support');
 			$("#font-pane").removeClass('support');
+			$(".support-puller").show();
 		}
 	});
 	$("#sublinks li a").click(function(){
