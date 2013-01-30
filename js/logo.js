47abd7f5323c4ce8a5f289b293e8547a22fd314d
Nithin Mohan <nithinmohan94@gmail.com>
2013-01-30 18:08:54 +0530

    color added;

--- js/logo.js
+++ js/logo.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('lettera');
+ var canvas = document.getElementById('lettera');
   var contexta = canvas.getContext('2d');
   var canvas = document.getElementById('letterm');
   var contextm = canvas.getContext('2d');
@@ -13,76 +13,82 @@ var canvas = document.getElementById('lettera');
   var counterClockwise = true;
   var  a=0, da=0.01 ;
 
-		context.translate(radius*4, 0);
-	function g() {
-		var x= 180, y = 02;
-		context.beginPath();
-		context.arc(x, y, radius, .5* Math.PI, 1* Math.PI, false);
-		context.moveTo(x,y+radius);
-		context.arc(x, y+2*radius, radius, 1.5*Math.PI, 3.5*Math.PI,false);
-		context.moveTo(x,3*radius);
-		context.arc(x, y+4*radius-1, radius,1.5* Math.PI, 0, true);
-		context.lineWidth =thick;
-      // add linear gradient
-      var grd = context.createLinearGradient(canvas.width*Math.cos(a), canvas.height*Math.sin(a), canvas.width*Math.cos(a+Math.PI), canvas.height*Math.sin(a+Math.PI));
+  context.translate(radius*4, 0);
+  contexta2.translate(radius*6+2*thick, 0);
+  contexta.translate(radius*2+thick,0);
+  contextm.translate(radius*8+2*thick,0);
+  function g() {
+    /////ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg////
+    var x= 180, y = 02;
+    context.beginPath();
+    context.arc(x, y, radius, .5* Math.PI, 1* Math.PI, false);
+    context.moveTo(x,y+radius);
+    context.arc(x, y+2*radius, radius, 1.5*Math.PI, 3.5*Math.PI,false);
+    context.moveTo(x,3*radius);
+    context.arc(x, y+4*radius-1, radius,1.5* Math.PI, 0, true);
+    context.lineWidth =thick;
+        var grd = context.createLinearGradient(radius*5+Math.cos(a), Math.sin(a), radius*5+Math.cos(a+Math.PI), Math.sin(a+Math.PI));
       // light blue
-      grd.addColorStop(0, '#8ED600');   
+      grd.addColorStop(0, '#831228');   
       // dark blue
-      grd.addColorStop(1, '#0044FF');
-      context.strokeStyle = grd;
+        grd.addColorStop(1, '#9e1630');
+        context.strokeStyle = grd;
+        context.stroke();
+        context.closePath();
+        ///mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm//////
+        xm=0,ym=0;
+        contextm.beginPath();
+    contextm.arc(xm+2*radius, ym+2*radius, radius, .80* Math.PI, 0* Math.PI, false);
+    contextm.arc(xm+4*radius, ym+2*radius, radius, 1* Math.PI, .5* Math.PI, false);
+    contextm.lineWidth =thick;
+    var grdm = contextm.createLinearGradient(canvas.width/2*Math.cos(a), canvas.height/2*Math.sin(a), canvas.width/2*Math.cos(a+Math.PI), canvas.height/2*Math.sin(a+Math.PI));
+        grdm.addColorStop(0, '#00b7ec');         
+      grdm.addColorStop(1, '#0067c1');
+        contextm.strokeStyle = grdm;
+    contextm.stroke();
+    contextm.closePath();
+        ////rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr////////////////////
+    xr=0,yr=0
+    contextr.beginPath();
+    contextr.arc(xr+2*radius, yr+2*radius, radius, 1.20* Math.PI, .67* Math.PI, false);
+    moveTo(xr+2*radius, yr+3*radius)
+    contextr.arc(xr+2*radius, yr+4*radius, radius, 1.5* Math.PI, 0* Math.PI, false);
+     var grdr = contextr.createLinearGradient(canvas.width*Math.cos(a), canvas.height*Math.sin(a), canvas.width*Math.cos(a+Math.PI), canvas.height*Math.sin(a+Math.PI));
+     
+        grdr.addColorStop(0, '#ed1d24');   
+      
+        grdr.addColorStop(1, '#b8171c');
+    contextr.lineWidth =thick;
+    contextr.strokeStyle = grdr;
+    contextr.stroke();
 
-      context.stroke();
-      context.closePath();
-      a+=da;
+    ///aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa//////////////////
+    xa=0,ya=0        
+        contexta.beginPath();
+    contexta.arc(xa+2*radius, ya+2*radius, radius, 0, 2* Math.PI, false);
+    contexta.arc(xa+4*radius, ya+2*radius+1, radius, 1* Math.PI, .5* Math.PI, true);
+    contexta.lineWidth =thick;
+    var grda = contexta.createLinearGradient(canvas.width*Math.cos(a), canvas.height*Math.sin(a), canvas.width*Math.cos(a+Math.PI), canvas.height*Math.sin(a+Math.PI));
+        grda.addColorStop(0, '#89c540');   
+        grda.addColorStop(1, '#00b04d');
+        contexta.strokeStyle = grda;
+        contexta.stroke();
+      contexta.closePath();
 
+        ///a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2//////////////////
+    xa2=0,ya2=0
+        contexta2.beginPath();
+    contexta2.arc(xa2+2*radius, ya2+2*radius, radius, 0, 2* Math.PI, false);
+    contexta2.arc(xa2+4*radius, ya2+2*radius+1, radius, 1* Math.PI, .5* Math.PI, true);
+    contexta2.lineWidth =thick;
+    var grda2 = contexta2.createLinearGradient(canvas.width*Math.cos(a), canvas.height*Math.sin(a), canvas.width*Math.cos(a+Math.PI), canvas.height*Math.sin(a+Math.PI));
+        grda2.addColorStop(0, '#f89a1c');   
+      
+        grda2.addColorStop(1, '#ee2524');
+        contexta2.strokeStyle = grda2;
+    contexta2.stroke();
+    contexta2.closePath();
+        a+=da;
       setTimeout('g()',2);
    }
     g();
-
-xm=0,ym=0
-
-        contextm.translate(radius*8+2*thick, 0);
-         contextm.beginPath();
-		contextm.arc(xm+2*radius, ym+2*radius, radius, .80* Math.PI, 0* Math.PI, false);
-		//moveTo(xm+4*radius, ym+3*radius)
-		contextm.arc(xm+4*radius, ym+2*radius, radius, 1* Math.PI, .5* Math.PI, false);
-
-contextm.lineWidth =thick;
-
- contextm.strokeStyle = '#451234';
-		contextm.stroke();
-		contextm.closePath();
-////////////aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-         xa=0,ya=0
-        contexta.translate(radius*2+thick,0);
-         contexta.beginPath();
-		contexta.arc(xa+2*radius, ya+2*radius, radius, 0, 2* Math.PI, false);
-		contexta.arc(xa+4*radius, ya+2*radius+1, radius, 1* Math.PI, .5* Math.PI, true);
-		contexta.lineWidth =thick;
-		contexta.stroke();
-		contexta.closePath();
-		/////
-//////////////a2a2a2a2aa2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2
-		xa2=0,ya2=0
-        contexta2.translate(radius*6+2*thick, 0);
-        contexta2.beginPath();
-		contexta2.arc(xa2+2*radius, ya2+2*radius, radius, 0, 2* Math.PI, false);
-		contexta2.arc(xa2+4*radius, ya2+2*radius+1, radius, 1* Math.PI, .5* Math.PI, true);
-		contexta2.lineWidth =thick;
-		contexta2.stroke();
-		contexta2.closePath();
-
-/////////////rrrrrrrrrrrrrrrrrrrrrrrr///////////////////
-xr=0,yr=0
-var canvas = document.getElementById('letterr');
-	var contextr = canvas.getContext('2d');
-         contextr.beginPath();
-		contextr.arc(xr+2*radius, yr+2*radius, radius, 1.20* Math.PI, .67* Math.PI, false);
-		moveTo(xr+2*radius, yr+3*radius)
-		contextr.arc(xr+2*radius, yr+4*radius, radius, 1.5* Math.PI, 0* Math.PI, false);
-
-contextr.lineWidth =thick;
-contextr.strokeStyle = '#451234';
-		contextr.stroke();
-
-//////mmmmmmmmmmmmmmmmmmmmmmmmmmmm/////////////
