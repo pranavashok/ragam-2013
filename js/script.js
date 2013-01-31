0722dcafefa8bcf822dbff465bda4a2dfc0bdd11
Sachin Krishnan T V <sachu92@gmail.com>
2013-01-31 21:43:26 +0530

    Fixed loading animation

--- js/script.js
+++ js/script.js
@@ -10,69 +10,58 @@ function setCssL(a){
 	$(a).css({left: vleft, top: vtop});
 }
 (function(window,undefined){
-
-    // Prepare
-    var History = window.History; // Note: We are using a capital H instead of a lower h
-    if ( !History.enabled ) {
-         // History.js is disabled for this browser.
-         // This is because we can optionally choose to support HTML4 browsers or not.
-        return false;
-    }
-
+    	// Prepare
+	var History = window.History; // Note: We are using a capital H instead of a lower h
+    	if ( !History.enabled ) {
+         	// History.js is disabled for this browser.
+        	 // This is because we can optionally choose to support HTML4 browsers or not.
+        	return false;
+    	}
 	$(function(){
 		var w = $(window).width();
 		var h = $(window).height();
 		// Bind to StateChange Event
-		$(window).bind('load',function() {
+		/*$(window).bind('load',function() {
 	        var State = History.getState(), // Note: We are using History.getState() instead of event.state
 	        	rootUrl = History.getRootUrl(),
 	        	relativeUrl = State.url.replace(rootUrl+subDir, '');
 	        if(relativeUrl!="/")
 				History.pushState(null, null, relativeUrl);
-		});
-	    $(window).bind('statechange',function(){ // Note: We are using statechange instead of popstate
-	        var State = History.getState(), // Note: We are using History.getState() instead of event.state
+		});*/
+		$("#mainmenu-pane").attr("class","loading");
+		$("#font-pane").attr("class","loading");
+		History.pushState(null, "Ragam 2013", "");
+	    	$(window).bind('statechange',function(){ // Note: We are using statechange instead of popstate
+	        	var State = History.getState(), // Note: We are using History.getState() instead of event.state
 	        	rootUrl = History.getRootUrl(),
 	        	relativeUrl = State.url.replace(rootUrl+subDir+'/', '');
 	        	//History.log(State.data, State.title, State.url);
-	        if(relativeUrl=="" && $("#mainmenu-pane").attr("class")=="moveout") {
-	        	$("#mainmenu-pane").attr("class", "movein");
-				$("#font-pane").attr("class", "movein");	     
-				$("#content-pane").attr("class", "rotatein");
-				$("#submenu-pane").attr("class", "rotatein");   	
-	        } 
-	        else {
-	        	$("#mainmenu-pane").attr("class", "moveout");
+	        	if(relativeUrl=="") {
+				if($("#mainmenu-pane").attr("class")=="moveout") {
+		        		$("#mainmenu-pane").attr("class", "movein");
+					$("#font-pane").attr("class", "movein");	     
+					$("#content-pane").attr("class", "rotatein");
+					$("#submenu-pane").attr("class", "rotatein");   	
+				}else if($("#mainmenu-pane").attr("class")=="pane") {
+					$("#mainmenu-pane").attr("class", "loading");
+					$("#font-pane").attr("class", "loading");
+					History.pushState(null, "Ragam 2013", "");			
+				}
+	        	}else{
+				setCssL('#font-pane');
+				setCssR('#mainmenu-pane');
+	        		$("#mainmenu-pane").attr("class", "moveout");
 				$("#font-pane").attr("class", "moveout");
-	        	loc=relativeUrl.split("/");
-	        	heading.innerHTML = loc[0];
-	        }   
-	    });
-
-		$("#mainmenu-pane").attr("class", "loading");
-		$("#font-pane").attr("class", "loading");
-		History.pushState(null, "Ragam 2013", "");
+				$("#followlinks").animate({opacity:'0'});
+		        	loc=relativeUrl.split("/");
+	        		heading.innerHTML = loc[0];
+		        }   	
+		});
 		$("#mainlinks a").click(function(e){
 			e.preventDefault();
 		});
 		$("#mainlinks li").click(function(){
 			History.pushState(null, $(this).attr("title") + " | Ragam 2013", $(this).attr("title"));
-			/*$.ajax("rsublinks.php", {
-			dataType : "json",
-			data: {"cat":$(this).attr('title')},
-			type: "POST",
-			success: function (d) {
-				if(d[0]==1)
-				{
-				// load content to hidden div	
-				}
-			}
-			});*/
-			setCssL('#font-pane');
-			setCssR('#mainmenu-pane');
-			$("#font-pane").attr("class", "moveout");
-			$("#mainmenu-pane").attr("class", "moveout");
-			$("#followlinks").animate({opacity:'0'});
 		});
 		$("#arrow-up").click(function(){
 			$("#wrapper").attr("class", "support-up");
