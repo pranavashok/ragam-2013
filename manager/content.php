eeee76359a95874a45a6ed9d91a965603986c153
Sachin Krishnan <sachu92@gmail.com>
2013-02-23 03:43:02 +0530

    eventreg (4)

--- manager/content.php
+++ manager/content.php
@@ -4,10 +4,11 @@ require_once("initdb.php");
 $event = $_POST['event'];
 
 $event = preg_replace('/_/', '$1 $2', $event);
-$query1=$mysqli->query("SELECT name,longdesc,contacts,shortdesc FROM events WHERE name='$event'");
+$query1=$mysqli->query("SELECT name,code,longdesc,contacts,shortdesc FROM events WHERE name='$event'");
 
 $eve = $query1->fetch_assoc();
 $data['name']=$eve['name'];
+$data['code']=$eve['code'];
 $content = preg_replace("/<img[^>]+\>/i", "", $eve['longdesc']); 
 $data['content']=$content."<br/><br/><strong>Contacts</strong><br/><br/>".$eve['contacts'];
 $data['content']=preg_replace("/\|\|sec\|\|/","<br/><br/><strong>",$data['content']);
