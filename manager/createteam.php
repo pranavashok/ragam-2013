5fcff248468822c613a535c98896a4d57a9a4399
Pranav Ashok <pranavashok@gmail.com>
2013-02-23 18:16:23 +0530

    Fixed createteam.php

--- manager/createteam.php
+++ manager/createteam.php
@@ -4,22 +4,26 @@ require_once("initdb.php");
 $event_id = $mysqli->real_escape_string($_POST['event']);
 $teamleader_id = $mysqli->real_escape_string($_POST['teamleader']);
 $teammemberlist = $mysqli->real_escape_string($_POST['teammembers']);
-$teammembers = $teammemberlist.split(",");
+$teammembers = explode(",", $teammemberlist);
 
-$query = $mysqli->query("SELECT max(team_id) FROM team WHERE event_id='$event_id'");
+$query = $mysqli->query("SELECT max(team_id) AS maxteamid FROM team WHERE event_id='$event_id'");
 $row = $query->fetch_assoc();
-if($row)
-	$maxteamid = $row['team_id'];
+if($row['maxteamid']!=NULL)
+	$maxteamid = $row['maxteamid'];
 else
 	$maxteamid = 100;
 $maxteamid++;
 
 /* Creating query to insert into table */
 $string = "INSERT INTO `team` VALUES ('$event_id', '$maxteamid', '$teamleader_id', '$teamleader_id')";
+
 for($i = 0; $i < count($teammembers)-1; $i++) {
 	$string .= ", ('$event_id', '$maxteamid', '$teamleader_id', '$teammembers[$i]')";
 }
 $string .= ";";
+
+echo $string;
+
 if($mysqli->query($string)) {
 	$success = 1;
 	$msg = "Your team has been registered. Your Team ID is $event_id$maxteamid";
