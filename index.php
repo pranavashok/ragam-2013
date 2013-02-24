9312ddda1f33c780b1e483e9b357dcce41cf3c2b
Sachin Krishnan <sachu92@gmail.com>
2013-02-24 21:04:04 +0530

    Added title to index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+	<title>Ragam 2013 | National Institute of Technology, Calicut</title>
 	<?php include 'root.php' ?>
 	<link href="<?php echo $subDir; ?>css/reset.css" rel="stylesheet"/>
 	<link href="<?php echo $subDir; ?>css/style.css" rel="stylesheet"/>
