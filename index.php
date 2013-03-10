2e5710496905b2d1e38f15f5353c59962a3faebf
Pranav Ashok <pranavashok@gmail.com>
2013-03-11 03:46:33 +0530

    ticker

--- index.php
+++ index.php
@@ -292,10 +292,7 @@
 			<div id="supportpane_top">
 				<div id="ticker">
 					<div>The event registration has started. You can click on the <span class="symbol" style="vertical-align:middle">M</span> sign on the respective event page to register</div>
-					<div>The Ragam 2013 <strong><a href="<?php echo $subDir; ?>schedule.html">schedule</a></strong> has been updated on Feb 28th.</div>
-					<div>Entries for the national short film contest, <strong><a href="<?php echo $subDir; ?>Events/General/Take_One">Take One</a></strong>, should be sent on or before March 5th</div>
-					<div>Those interested in participating for <strong><a href="<?php echo $subDir; ?>Events/Dance/Choreo_Nite">Choreo Nite</a></strong> and <strong><a href="<?php echo $subDir; ?>Events/General/Couture_Boulevard">Couture Boulevard</a></strong> are to mail their previous performances on or before March 7th</div>
-					<div>Those interested in participating for <strong><a href="<?php echo $subDir; ?>Events/Music/Amplified">Amplified</a></strong>, <strong><a href="<?php echo $subDir; ?>Events/Music/Acoustics">Acoustics</a></strong> and <strong><a href="<?php echo $subDir; ?>Events/Music/Swara_Raaga">Swara Raaga</a></strong> are to mail their previous performances on or before March 7th</div>
+					<div>The Ragam 2013 <strong><a href="<?php echo $subDir; ?>schedule.html">schedule</a></strong> has been updated on Mar 8th.</div>
 				</div>
 				<form id="searchform" action="" method="">
 					<span id="search_panel">
