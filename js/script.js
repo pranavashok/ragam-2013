d0239db6dbea2fbcb28228fb3f030eeb59bac734
Pranav Ashok <ashok@in.tum.de>
2020-04-13 17:39:29 +0200

    rooturl issue fix?

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ function lookup(inputString) {
 		History.Adapter.bind(window, 'statechange', function() { // Note: We are using statechange instead of popstate
 			var State = History.getState(), // Note: We are using History.getState() instead of event.state
 				rootUrl = History.getRootUrl(),
-				relativeUrl = State.url.replace(rootUrl + subDir + '/', '');
+				relativeUrl = State.url.replace(rootUrl + subDir, '');
 			//History.log(State.data, State.title, State.url);
 			if (relativeUrl == '') {
 				if ($('#mainmenu-pane').attr('class') == 'moveout') {
